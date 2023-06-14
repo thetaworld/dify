a2e98355164f7b120ba261baad10a1f3906a4ffc
zhangsiyuan11445 <zhangsiyuan11445@hellobike.com>
2023-06-14 17:41:12 +0800

    删除不必要的代码

--- web/context/dataset-detail.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'use-context-selector'
-import type { DataSet } from '@/models/datasets'
-
-const DatasetDetailContext = createContext<{ indexingTechnique?: string; dataset?: DataSet }>({})
-
-export default DatasetDetailContext
